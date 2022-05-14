775d73d6ab4fdbcb416c26f4a75829edcaddcfc0
ArchitectGuan <architectguan@qq.com>
2022-05-14 15:19:57 +0800

    more CRUD operations

--- s15-node-express/src/controllers/todos.ts
+++ s15-node-express/src/controllers/todos.ts
@@ -16,3 +16,38 @@ export const createTodo: RequestHandler = (req, res, next) => {
     createdTodo: newTodo
   });
 };
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.status(200).json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const updateText = (req.body as { text: string }).text;
+  const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error('Could not find todo!');
+  }
+
+  TODOS[todoIndex].text = updateText;
+
+  res.json({ message: 'Updated!', updatedTodo: TODOS[todoIndex] });
+};
+
+export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error('Could not find todo!');
+  }
+  const todo = TODOS.splice(todoIndex, 1);
+
+  res.json({
+    message: 'Todo deleted!',
+    todo
+  });
+};
